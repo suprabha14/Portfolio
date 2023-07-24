4e34c96fee94f169756956d7ab5c34cc5a86ed3b
suprabha14 <suprabhasahoo2002@gmail.com>
2023-07-25 00:37:20 +0530

    I made a portfolio

---
+++ index.js
@@ -0,0 +1,5 @@
+document.querySelector('.close').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", ()=> {
+    document.querySelector('.options').classList.toggle('options-go');
+    
+});
