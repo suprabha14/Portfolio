55812c043b2ba906b336e015a24f7b8fc55946f3
suprabha14 <suprabhasahoo2002@gmail.com>
2023-07-31 00:05:14 +0530

    My portfolio

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 document.querySelector('.close').style.display = 'none';
 document.querySelector('.hamburger').addEventListener("click", ()=> {
     document.querySelector('.options').classList.toggle('options-go');
-    
+    if(document.querySelector('.options').classList.contains('options-go')) {
+        document.querySelector('.ham').style.display = "inline";
+        document.querySelector('.close').style.display = "none";
+    }
+    else {
+        document.querySelector('.ham').style.display = "none";
+        setTimeout(()=> {
+            document.querySelector('.close').style.display = 'inline'
+        }, 300);
+        
+    }
 });
